06017d1f044af7d669aefe673c96368e59663eb2
Alexthemediocre <65747823+Alexthemediocre@users.noreply.github.com>
2022-10-08 11:38:59 -0400

    Update to newer version

--- bruteForce.js
+++ bruteForce.js
@@ -4,13 +4,27 @@
 const fs = require('fs');
 
 const {
+    /** The url of the webhook to log to */
     WEBHOOK_URL,
+    /** The id of the challenge that is being brute forced */
     challenge_id,
+    /** The headers that will be sent to the Radford site. Must include token and therefore should remain private. */
     radford_headers,
+    /** The time to wait in between questions */
     time_after_incorrect,
+    /** The time to wait after being ratelimited or experiencing a network error */
     time_after_ratelimit
 } = require('./config.json');
 
+/**
+ * Makes a submission to the given challenge with the given flag.
+ * 
+ * Receiving anything other than a 2XX status code will make it return "ratelimited" to be safe.
+ * If it is an incorrect flag, it returns "incorrect", else "done".
+ * @param {number} challenge_id 
+ * @param {string} submission 
+ * @returns {Promise<'ratelimited' | 'incorrect' | 'done'>}
+ */
 async function submit(challenge_id, submission) {
     let res = await fetch("https://rusecurectf.radford.edu/api/v1/challenges/attempt", {
         "headers": radford_headers,
@@ -25,6 +39,10 @@ async function submit(challenge_id, submission) {
     return 'done';
 }
 
+/**
+ * Error handler for web requests using fetch
+ * @param {Promise<Response>} req 
+ */
 async function handleFetch(req) {
     try {
         await req;
@@ -36,62 +54,71 @@ async function handleFetch(req) {
     }
 }
 
-let bruteForceLog = fs.createWriteStream('bruteForceLog_md5s.txt');
+/** Log file */
+let bruteForceLog = fs.createWriteStream('bruteForceLog.txt');
 (async () => {
-    console.log('65536 total');
+    console.log('100,000 total');
+    /** Starting number - allows the process to be stopped and started again without repeating submissions */
     let initialNum = +process.argv[2] || 0;
 
-    for (let i = initialNum; i < 0xffff; i++) {
-        let s = i.toString(16).padStart(4, '0');
-        console.log(`string: ${s}, index: ${i}`);
+    for (let i = initialNum; i < 100_000; i++) {
+        let str = i.toString().padStart(5, "0");
+        console.log(str);
 
+        // Assumes that it is ratelimited if it encounters a network error to give it time
+        // to resolve itself
         let res = 'ratelimited';
         try {
-            res = await submit(challenge_id, s);
+            res = await submit(challenge_id, str);
         } catch (err) {
             console.error(err);
             let str = `\n[${new Date().toISOString()}] Error submitting answer:\n${err.stack}\n`;
             if (err.cause) str += `Error cause:\n${err.cause?.stack ?? err.cause}\n`;
             bruteForceLog.write(str + '\n');
+            // Log network errors to the webhook
             await handleFetch(fetch(WEBHOOK_URL, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    content: `<@370287366834880512> <@600010784453558331>: Error\nChallenge ID: ${challenge_id}\nIndex: ${i}\nString: ${s}\nError: \`\`\`js\n${err.stack}\n\`\`\``
+                    content: `<@370287366834880512> <@600010784453558331>: Error\nChallenge ID: ${challenge_id}\nIndex: ${i}\nString: ${str}\nError: \`\`\`js\n${err.stack}\n\`\`\``
                 })
             }));
         }
         if (res === 'ratelimited') {
+            // Log being ratelimited to the webhook
             await handleFetch(fetch(WEBHOOK_URL, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    content: `<@370287366834880512> <@600010784453558331>: Ratelimited\nChallenge ID: ${challenge_id}\nIndex: ${i}\nString: ${s}`
+                    content: `<@370287366834880512> <@600010784453558331>: Ratelimited\nChallenge ID: ${challenge_id}\nIndex: ${i}\nString: ${str}`
                 })
             }));
         }
 
         console.log(res);
-        bruteForceLog.write(`string: ${s}, index: ${i}, response: ${res}\n`);
+        bruteForceLog.write(`string: ${str}, index: ${i}, response: ${res}\n`);
+        // Wait for the necessary amount of time
         if (res !== 'done') await new Promise(resolve => setTimeout(() => resolve(), res === 'incorrect' ? time_after_incorrect : time_after_ratelimit));
         else break;
 
-        if (i % 100 === 0) {
+        // Log to the webhook every 200 submissions
+        if (i % 200 === 0) {
             await handleFetch(fetch(WEBHOOK_URL, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    content: `Routine update:\nChallenge ID: ${challenge_id}\nIndex: ${i}\nString: ${s}`
+                    content: `Routine update:\nChallenge ID: ${challenge_id}\n\nIndex: ${i}\nString: ${str}\n${"-".repeat(10)}\n${((i / 65536) * 100).toFixed(3)}% Completed`
                 })
             }));
         }
 
+        // Repeat submission if it didn't go through
         if (res === 'ratelimited') i--;
     }
     bruteForceLog.close();
--- bruteForceBot.js
+++ bruteForceBot.js
@@ -5,7 +5,9 @@ const child_process = require('child_process');
 
 const config = require('./config.json');
 
+/** Log file */
 let botLogs = fs.createWriteStream('botLogs.txt');
+fs.mkdirSync('output_logs', {recursive: true});
 
 const bot = new Eris.Client(config.TOKEN, {intents: ["allNonPrivileged", "guildMessages", "directMessages", "guildMembers"]});
 
@@ -21,10 +23,18 @@ let proc = undefined;
 
 function startCtf(index) {
    if (proc && !proc.killed) proc.kill();
-   let args = ["bruteForce_md5s.js"];
+   let args = ["bruteForce.js"];
    if (index) args.push(index);
-   proc = child_process.spawn('node', args);
-   proc.once('exit', (code, signal) => botLogs.write(`[${new Date().toISOString()}] Brute force script stopped. Code ${code}, signal: ${signal}\n`));
+   let localProc = child_process.spawn('node', args, {stdio: 'pipe'});
+   localProc.once('exit', (code, signal) => {
+      let time = new Date().toISOString();
+      botLogs.write(`[${time}] Brute force script stopped. Code ${code}, signal: ${signal}\n`);
+      time = time.replaceAll(':', '_');
+      botLogs.write(`Formatted time: ${time}\n`);
+      fs.writeFileSync(`output_logs/${time}_stdout.txt`, localProc.stdout.read());
+      fs.writeFileSync(`output_logs/${time}_stderr.txt`, localProc.stderr.read());
+   });
+   proc = localProc;
 }
 
 function endCtf() {
@@ -55,16 +65,40 @@ bot.on('messageCreate', (msg) => {
             endCtf();
             msg.channel.createMessage('Ended');
             break;
+         case '*outputlogs':
+         case '*output-logs':
+            let times = fs.readdirSync('output_logs').filter(v => v.endsWith('_stdout.txt')).map(v => v.substring(0, v.lastIndexOf('_')));
+            msg.channel.createMessage(`Available times:\n${times.join(' | ') || 'None'}`);
+            break;
+         case '*ctfoutput':
+         case '*ctf-output':
+            let type = args[1]?.toLowerCase();
+            if (type !== 'stdout' && type !== 'stderr') {
+               msg.channel.createMessage('You must specify to get either the stdout or stderr logs with the first argument.');
+               break;
+            }
+            let time = args[2];
+            if (!time) {
+               msg.channel.createMessage('You must specify the time of the log. This can be gotten from the bot logs.');
+               break;
+            }
+            let file = `${time}_${type}.txt`;
+            try {
+               msg.channel.createMessage(`${type} logs at time ${time}:`, [{name: file, file: fs.readFileSync(`output_logs/${file}`)}]);
+            } catch {
+               msg.channel.createMessage(`${file} was not found.`);
+            }
+            break;
          case '*ctflogs':
          case '*ctf-logs':
             try {
-               msg.channel.createMessage({content: 'Logs:'}, [{name: 'logs.txt', file: fs.readFileSync("bruteForceLog_md5s.txt")}]);
+               msg.channel.createMessage({content: 'Logs:'}, [{name: 'logs.txt', file: fs.readFileSync("bruteForceLog.txt")}]);
             } catch (err) {
                msg.channel.createMessage("Looks like the log file hasn't been made yet");
             }
             break;
          case '*help':
-            msg.channel.createMessage('Commands:\n`*start [index]`\n`*end`\n`*ctflogs|*ctf-logs`\n`*eval [...content]`\n`*update-config entryName jsonValue`\n`*botlogs|*bot-logs`');
+            msg.channel.createMessage('Commands:\n`*start [index]`\n`*end`\n`*ctflogs|*ctf-logs`\n`*eval [...content]`\n`*update-config entryName jsonValue`\n`*botlogs|*bot-logs`\n`*outputlogs|*output-logs`\n`*ctfoutput|*ctf-output {stdout|stderr} time`');
             break;
          case '*eval':
             if (msg.author.id !== '370287366834880512' && msg.author.id !== '600010784453558331') break;
